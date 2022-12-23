e25c051e187f22c6ea52f34b82556b9ab4a8dfdb
sergei <yasen20008@gmail.com>
2022-12-23 13:26:29 +0300

    step 4.3.3

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ import path from 'path';
 import * as yaml from 'js-yaml';
 
 const readJson = (filePath) => {
-  const data = fs.readFileSync(filePath);
+  const data = fs.readFileSync(path.resolve(filePath));
   if (filePath.extname === '.json') {
     return JSON.parse(data);
   }
@@ -13,15 +13,14 @@ const readJson = (filePath) => {
 };
 
 const filesCompare = (filepath1, filepath2) => {
-  const data1 = readJson(path.resolve(filepath1));
-  const data2 = readJson(path.resolve(filepath2));
+  const data1 = readJson(filepath1);
+  const data2 = readJson(filepath2);
   const data1Keys = Object.keys(data1);
   const data2Keys = Object.keys(data2);
   const keyIntersection = _.intersection(data1Keys, data2Keys);
   const key1Difference = _.difference(data1Keys, data2Keys);
   const key2Difference = _.difference(data2Keys, data1Keys);
   let result = [];
-
   const keyIntersectionResult = keyIntersection.map((key) => {
     if (data1[key] === data2[key]) {
       return [`  ${key}: ${data1[key]}`];
@@ -34,11 +33,7 @@ const filesCompare = (filepath1, filepath2) => {
   const key2DifferenceResult = key2Difference.map((key) => [
     `+ ${key}: ${data2[key]}`,
   ]);
-  result = [
-    ...keyIntersectionResult,
-    ...key1DifferenceResult,
-    ...key2DifferenceResult,
-  ].flat();
+  result = [...keyIntersectionResult, ...key1DifferenceResult, ...key2DifferenceResult].flat();
   return result.join('\n');
 };
 
